a6a50c26e87e629419df68eccca9d41f0a774c01
Vinicius Farias <102328663+ViniciusAlbanit@users.noreply.github.com>
2022-09-08 11:20:10 -0300

    Update classecarro.py

--- classecarro.py
+++ classecarro.py
@@ -5,18 +5,20 @@ class Carro:
     def __repr__(self):
         return f'Litros no tanque {self.obter_combustivel()}'
     
-    def adicionar_combustivel(self):
+    def adicionar_combustivel(self, add_combu):
         self.quantidade_combustivel = add_combu
     
     def obter_combustivel(self):
-        self.quantidade_combustivel
+      return  self.quantidade_combustivel
 
-    def andar(self):
-        while distancia < 0:
-            add_combu -= 0,20
+    def andar(self, distancia):
+        while distancia > 0:
+            self.quantidade_combustivel -= 0.20
             distancia -= 1
 
 add_combu = float(input('Quanto de combustivél você quer adicionar: '))
 distancia = float(input('Distancia que irá percorrer: '))
 car = Carro()
+car.adicionar_combustivel(add_combu)
+car.andar(distancia)
 print(car)
