e0bd63fb36b13295288a60dc38cf10e84b20bece
Vinicius Farias <102328663+ViniciusAlbanit@users.noreply.github.com>
2022-09-14 11:07:46 -0300

    Create classecasamento.py

---
+++ classecasamento.py
@@ -0,0 +1,14 @@
+class casamento:
+    def __init__(self, nome, anfitrioes):
+        self.nome = nome
+        self.anfitrioes = anfitrioes
+    
+    def __repr__(self):
+        return f'-'*20 + \
+               f'{casamen}'
+
+    def noivo(self):
+        self.anfitrioes = donos
+
+nome, donos = input().split()
+casamen = casamento(nome, donos)
