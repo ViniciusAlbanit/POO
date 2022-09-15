1daf65e918d08657e76fa88bac87dea3e3a372ae
Vinicius Farias <102328663+ViniciusAlbanit@users.noreply.github.com>
2022-09-15 11:16:17 -0300

    Update classecasamento.py

--- classecasamento.py
+++ classecasamento.py
@@ -1,1 +1,24 @@
-casamento = {}
+from posixpath import split
+cnoivo = set('noivo')
+cnoiva = set('noiva')
+nome, noivos = input(), split(';')
+while nome != 'ACABOU':
+    if noivos == 'noivo':
+        cnoivo.add(nome)
+    elif nome == 'ACABOU':
+        break
+    else:
+        cnoiva.add(nome)
+
+
+    
+    
+
+print(cnoivo)
+print(cnoiva)
+
+
+
+
+
+
