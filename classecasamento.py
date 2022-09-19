4d358a5a3f5578c075add51fc02d1682761f7c0b
Vinicius Farias <102328663+ViniciusAlbanit@users.noreply.github.com>
2022-09-19 15:05:41 -0300

    Update classecasamento.py

--- classecasamento.py
+++ classecasamento.py
@@ -1,23 +1,24 @@
 
-cnoivo = set('noivo')
-cnoiva = set('noiva')
-nome, noivos = input().split(';')
-while nome != 'ACABOU':
-    if noivos == 'noivo':
-        cnoivo.add(nome)
-    else:
-        cnoiva.add(nome)
+cnoivo = set()
+cnoiva = set()
+cnoivo.add('noivo')
+cnoiva.add('noiva')
+conv, dono = input().split(';')
+if len(conv) > 1:
+    while len(conv) > 1:
+        if dono == 'noivo':
+            cnoivo.add(conv)
+            conv, dono = input().split(';')
+        else: 
+            cnoiva.add(conv)
+            conv, dono = input().split(';')
+else:
+    print(cnoiva)
 
 
 print(cnoivo)
 print(cnoiva)
 
-
-linha = input().split(';')
-if len(linha) > 1:
-    .....
-else:
-    tchau!
 
 
 
