3dabe6cc3e2cf0118339805c4451b7dd4b9c647a
Vinicius Farias <102328663+ViniciusAlbanit@users.noreply.github.com>
2022-09-20 04:31:59 -0300

    Update classecasamento.py

--- classecasamento.py
+++ classecasamento.py
@@ -2,22 +2,44 @@ cnoivo = set()
 cnoiva = set()
 cnoivo.add('noivo')
 cnoiva.add('noiva')
-conv, dono = input().split(';')
-if len(conv) < 1:
+conv = input().split(';')
+if len(conv) < 2:
     print(cnoiva)
 else:
     while len(conv) > 1:
-        if dono == 'noivo':
-            cnoivo.add(conv)
-            conv, dono = input().split(';')
+        if conv[1] == 'noivo':
+            cnoivo.add(conv[0])
+            conv = input().split(';')
         else: 
-            cnoiva.add(conv)
-            conv, dono = input().split(';')
+            cnoiva.add(conv[0])
+            conv = input().split(';')
 
+final = cnoivo.union(cnoiva)
 
-print(cnoivo)
-print(cnoiva)
-
+print('-'*20)
+print(f'LISTA FINAL')
+print('-'*20)
+print(sorted(final))
+print()
+print('-'*20)
+print(f'APENAS NOIVA')
+print('-'*20)
+print(sorted(cnoiva))
+print()
+print('-'*20)
+print(f'APENAS NOIVO')
+print('-'*20)
+print(sorted(cnoivo))
+print()
+print('-'*20)
+print(f'POR AMBOS')
+print('-'*20)
+ambos = cnoivo.intersection(cnoiva)
+print(sorted(ambos))
+print()
+print('-'*20)
+print(f'POR APENAS UM DELES')
+print()
 
 
 
