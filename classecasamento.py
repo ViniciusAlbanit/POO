3c85601663af2834bcfba72cd14c55c397ee25be
Vinicius Farias <102328663+ViniciusAlbanit@users.noreply.github.com>
2022-09-20 06:12:36 -0300

    Update classecasamento.py

--- classecasamento.py
+++ classecasamento.py
@@ -2,7 +2,54 @@ cnoivo = set()
 cnoiva = set()
 conv = input().split(';')
 if len(conv) < 2:
-    print(cnoiva)
+    print('-'*20)
+    print(f'LISTA FINAL')
+    print('-'*20)
+    print()
+    lista = list(final)
+    lista.sort()
+    for items in lista:
+        print(items)
+    print()
+    print('-'*20)
+    print(f'APENAS NOIVA')
+    print('-'*20)
+    print()
+    anoiva = cnoiva.difference(cnoivo)
+    listanoiva = list(anoiva)
+    listanoiva.sort()
+    for items in listanoiva:
+        print(items)
+    print()
+    print('-'*20)
+    print(f'APENAS NOIVO')
+    print('-'*20)
+    print()
+    anoivo = cnoivo.difference(cnoiva)
+    listanoivo = list(anoivo)
+    listanoivo.sort()
+    for items in listanoivo:
+        print(items)
+    print()
+    print('-'*20)
+    print(f'POR AMBOS')
+    print('-'*20)
+    print()
+    ambos = cnoivo.intersection(cnoiva)
+    listaambos = list(ambos)
+    listaambos.sort()
+    for items in listaambos:
+        print(items)
+    print()
+    print('-'*20)
+    print(f'POR APENAS UM DELES')
+    print('-'*20)
+    print()
+    umdeles = cnoivo.symmetric_difference(cnoiva)
+    listaumdeles = list(umdeles)
+    listaumdeles.sort()
+    for items in listaumdeles:
+        print(items)
 else:
     while len(conv) > 1:
         if conv[1] == 'noivo':
@@ -13,7 +60,6 @@ else:
             conv = input().split(';')
 
 final = cnoivo.union(cnoiva)
-
 print('-'*20)
 print(f'LISTA FINAL')
 print('-'*20)
@@ -28,8 +74,9 @@ print(f'APENAS NOIVA')
 print('-'*20)
 print()
 anoiva = cnoiva.difference(cnoivo)
-for items in anoiva:
-    sorted(anoiva)
+listanoiva = list(anoiva)
+listanoiva.sort()
+for items in listanoiva:
     print(items)
 print()
 print('-'*20)
@@ -37,8 +84,9 @@ print(f'APENAS NOIVO')
 print('-'*20)
 print()
 anoivo = cnoivo.difference(cnoiva)
-for items in anoivo:
-    sorted(anoivo)
+listanoivo = list(anoivo)
+listanoivo.sort()
+for items in listanoivo:
     print(items)
 print()
 print('-'*20)
@@ -46,8 +94,9 @@ print(f'POR AMBOS')
 print('-'*20)
 print()
 ambos = cnoivo.intersection(cnoiva)
-for items in ambos:
-    sorted(ambos)
+listaambos = list(ambos)
+listaambos.sort()
+for items in listaambos:
     print(items)
 print()
 print('-'*20)
@@ -55,9 +104,11 @@ print(f'POR APENAS UM DELES')
 print('-'*20)
 print()
 umdeles = cnoivo.symmetric_difference(cnoiva)
-for items in umdeles:
-    sorted(umdeles)
+listaumdeles = list(umdeles)
+listaumdeles.sort()
+for items in listaumdeles:
     print(items)
+
 
 
 
