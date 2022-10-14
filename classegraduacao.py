ed205371fb2797a97c4efa384707863d6f73c51a
Vinicius Farias <102328663+ViniciusAlbanit@users.noreply.github.com>
2022-10-14 08:17:42 -0300

    Update classegraduacao.py

--- classegraduacao.py
+++ classegraduacao.py
@@ -2,13 +2,29 @@ class PosGraduacao():
     def __init__(self, instituicao, curso):
         self.instituicao = instituicao
         self.curso = curso
-    def get_curso(self):
-        self.curso
+
+    def get_curso(self, situacao):
+        if situacao == 'Aprovado':
+            return "Doutorado em " + self.curso
+        else:
+            return none
+
 class Doutorado(PosGraduacao):
-    def __init__(self, instituicao, curso, tese):
-        super().__init__(instituicao, curso)
-        self.__tese = None
+    def __init__(self, instituicao, curso, tese=None):
+        super().init(instituicao, curso)
+        self.__tese = tese
+
     def get_tese(self):
-        self.__tese
-    def set_tese():
+        return self.__tese
+
+    def set_tese(self, titulo):
+        self.__tese = titulo
+
+    def repr(self):
+        return (f'Instituição: {self.instituicao}\n' +
+                f'Curso......: {self.curso}\n' +
+                f'Tese.......: {self.__tese}')
+
+d1 = Doutorado('Impacta', 'ADS')
+print(d1.get_tese())
         
