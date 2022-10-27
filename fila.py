f799f0c188076f87b6ee9871c46b4885ca6b7b49
Vinicius Farias <102328663+ViniciusAlbanit@users.noreply.github.com>
2022-10-27 10:45:56 -0300

    Update fila.py

--- fila.py
+++ fila.py
@@ -3,28 +3,33 @@ class Fila:
         self.__itens = tamanho * [None]
         self.__inicio = 0
         self.__fim = 0
-    
+
+    def __repr__(self):
+        s = 'S ⇽ |'
+        for item in self.__itens:
+            s += f' {item} |'
+        s += '| ⇽ E'
+        return s
     def vazia(self):
         return self.__fim == self.__inicio
 
     def cheia(self):
-        return self.__fim == len(self.__itens)    
+        return self.__fim == len(self.__itens)
 
     def enfileira(self, valor):
         if self.cheia():
-            print('Operação invalida fila cheia')
+            print('Operação inválida! Fila cheia')
             exit()
         self.__itens[self.__fim] = valor
         self.__fim += 1
-    
-    def desenfileira(self, valor):
-        if self.__vazia():
-            print('Operação invalida fila cheia')
+    def desenfileira(self):
+        if self.vazia():
+            print('Operação inválida! Fila vazia')
             exit()
         item = self.__itens[self.__inicio]
         i = 0
-        while i <= fim-2:
-            self.__itens[i] = self.__itens[i + 1]
+        while i <= self.__fim-2:
+            self.__itens[i] = self.__itens[i+1]
             i += 1
         return item
 
